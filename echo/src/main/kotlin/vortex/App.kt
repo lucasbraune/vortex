100b82e9c05c479da14aa22c01129aa350eff290
Lucas Braune <lucasbraune@gamil.com>
2023-04-30 02:59:03 +0200

    Reimplement echo challenge with Node

--- echo/src/main/kotlin/vortex/App.kt
+++ echo/src/main/kotlin/vortex/App.kt
@@ -1,93 +1,34 @@
 package vortex
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-
-@Serializable
-data class Message(
-    @SerialName("src")
-    val source: String,
-    @SerialName("dest")
-    val destination: String,
-    val body: MessageBody,
-)
-
-@Serializable
-sealed interface MessageBody {
-    val messageId: Int?
-    val inReplyTo: Int?
-}
-
-@Serializable
-@SerialName("init")
-data class Init(
-    @SerialName("node_id")
-    val nodeId: String,
-    @SerialName("node_ids")
-    val nodeIds: List<String>,
-    @SerialName("msg_id")
-    override val messageId: Int? = null,
-    @SerialName("in_reply_to")
-    override val inReplyTo: Int? = null,
-): MessageBody
-
-@Serializable
-@SerialName("init_ok")
-data class InitOk(
-    @SerialName("msg_id")
-    override val messageId: Int? = null,
-    @SerialName("in_reply_to")
-    override val inReplyTo: Int? = null,
-): MessageBody
-
-@Serializable
-@SerialName("echo")
-data class Echo(
-    val echo: String,
-    @SerialName("msg_id")
-    override val messageId: Int? = null,
-    @SerialName("in_reply_to")
-    override val inReplyTo: Int? = null,
-): MessageBody
-
-@Serializable
-@SerialName("echo_ok")
-data class EchoOk(
-    val echo: String,
-    @SerialName("msg_id")
-    override val messageId: Int? = null,
-    @SerialName("in_reply_to")
-    override val inReplyTo: Int? = null,
-): MessageBody
+import vortex.protocol.Echo
+import vortex.protocol.EchoOk
+import vortex.protocol.Init
+import vortex.protocol.InitOk
+import vortex.protocol.MessageBody
+import vortex.protocol.Node
 
 fun main() {
-    val json = Json { ignoreUnknownKeys = true }
-    var nextMessageId = 1
-    while (true) {
-        val line = readLine() ?: return // End of input
-        val request: Message = json.decodeFromString(line)
+    EchoNode().serve()
+}
 
-        val responseBody = when (request.body) {
-            is Init -> InitOk(
-                messageId = nextMessageId++,
-                inReplyTo = request.body.messageId,
+class EchoNode: Node() {
+    override fun handleMessage(source: String, body: MessageBody) {
+        when (body.payload) {
+            is Init -> {
+                nodeId = body.payload.nodeId
+                nodeIds = body.payload.nodeIds
+                sendMessage(
+                    destination = source,
+                    payload = InitOk,
+                    inReplyTo = body.messageId,
+                )
+            }
+            is Echo -> sendMessage(
+                destination = source,
+                payload = EchoOk(body.payload.echo),
+                inReplyTo = body.messageId,
             )
-            is Echo -> EchoOk(
-                echo = request.body.echo,
-                messageId = nextMessageId++,
-                inReplyTo = request.body.messageId,
-            )
-            else -> throw Exception("Unexpected message body: ${request.body}")
+            else -> throw Exception("Unexpected message payload: ${body.payload}")
         }
-        val response = Message(
-            source = request.destination,
-            destination = request.source,
-            body = responseBody
-        )
-
-        println(json.encodeToString(response))
     }
 }
--- echo/src/main/kotlin/vortex/protocol/Protocol.kt
+++ echo/src/main/kotlin/vortex/protocol/Protocol.kt
@@ -1,9 +1,34 @@
 package vortex.protocol
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 data class MessageBody(
     val messageId: Int?,
     val inReplyTo: Int?,
     val payload: MessagePayload,
 )
 
+@Serializable
 sealed interface MessagePayload
+
+@Serializable
+@SerialName("init")
+data class Init(
+    @SerialName("node_id")
+    val nodeId: String,
+    @SerialName("node_ids")
+    val nodeIds: List<String>,
+): MessagePayload
+
+@Serializable
+@SerialName("init_ok")
+object InitOk: MessagePayload
+
+@Serializable
+@SerialName("echo")
+data class Echo(val echo: String): MessagePayload
+
+@Serializable
+@SerialName("echo_ok")
+data class EchoOk(val echo: String): MessagePayload
